6450478450b0f279fd19af9e4b2fbe6d99b43b9e
Younes Halim <younes.halim@gmail.com>
2024-02-23 21:58:05 -0500

    new layers added

--- layer.py
+++ layer.py
@@ -3,9 +3,16 @@ from enum import IntEnum, auto
 
 class Layer(IntEnum):
     BACKGROUND = auto()
+    UI = auto()
+    SCORE = auto()
+    NEW_GAME = ()
     ASTEROID = auto()
+    GAME_OVER = auto()
     ALIEN = auto()
     PLAYER = auto()
     WEAPON = auto()
     EXPLOSION = auto()
-    UI = auto()
+    PARTICLE = auto()
+
+
+
