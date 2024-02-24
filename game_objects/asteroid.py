4ab00fbf6506a4cc336c1d0b007553d5eacf86e6
Younes Halim <younes.halim@gmail.com>
2024-02-23 19:59:55 -0500

    Added health to asteroid

--- game_objects/asteroid.py
+++ game_objects/asteroid.py
@@ -9,7 +9,13 @@ from layer import Layer
 
 
 class Asteroid(pygame.sprite.Sprite):
-    def __init__(self, *groups: AbstractGroup, x=None, y=None):
+
+    def __init__(self,
+                 *groups: AbstractGroup,
+                 asteroid_scale: tuple = (64, 64),
+                 x: int = None,
+                 y: int = None):
+
         self._layer = Layer.ASTEROID
         self.images = []
         terminate = False
@@ -18,7 +24,7 @@ class Asteroid(pygame.sprite.Sprite):
             try:
                 file_name = f'Asteroid-A-10-{counter}'
                 asteroid_sprite = assets.get_sprite(file_name)
-                self.images.append(asteroid_sprite)
+                self.images.append(pygame.transform.scale(asteroid_sprite, (asteroid_scale[0], asteroid_scale[1])))
                 counter += 1
             except KeyError:
                 terminate = True
@@ -26,6 +32,7 @@ class Asteroid(pygame.sprite.Sprite):
         self.image = self.images[0]
         self.rect = self.image.get_rect(topleft=(random.randint(0, configs.SCREEN_WIDTH - self.image.get_width()), 0))
         self.mask = pygame.mask.from_surface(self.image)
+        self.__health = 100
         self.animation_speed = 0
         super().__init__(*groups)
 
@@ -39,7 +46,19 @@ class Asteroid(pygame.sprite.Sprite):
         self.image = self.images[index]
 
     def __asteroid_drop(self):
-        self.rect.y += 2
+        self.rect.y += 3.5
         if self.rect.y >= configs.SCREEN_HEIGHT:
-            self.rect.y = 0
-            self.rect.x = random.randint(0, configs.SCREEN_WIDTH - self.image.get_width())
+            self.kill()
+
+    def asteroid_hit(self, damage: int = 30):
+        self.__health -= damage
+        self.rect.y -= 10.5
+        if self.__health <= 0:
+            self.kill()
+
+    def get_health(self) -> int:
+        return self.__health
+
+    def is_destroyed(self) -> bool:
+        return True if self.__health <= 0 else False
+
