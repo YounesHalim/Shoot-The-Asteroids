707e6cd5e3f621f9abd1879757f25fce0fdafbe2
Younes Halim <younes.halim@gmail.com>
2024-02-26 10:31:57 -0500

    improved spaceship state handling

--- game_objects/spaceship.py
+++ game_objects/spaceship.py
@@ -4,25 +4,25 @@ from pygame.sprite import AbstractGroup
 import assets
 import configs
 from layer import Layer
+from abc import ABC, abstractmethod
 
 
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup):
         self._layer = Layer.PLAYER
         self.images = [
-            pygame.transform.scale(assets.get_sprite('tile000'), (100, 100))
+            pygame.transform.scale(assets.get_sprite('idle'), (100, 100)),
         ]
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect(topleft=((configs.SCREEN_WIDTH // 2) - 32, configs.SCREEN_HEIGHT - 70))
         self.move = 4
-        self.is_idle = True
-        self.tilting_right = False
-        self.tilting_left = False
         self.health = 100
-        self.last_frame_update = 0  # Store the time of the last frame update
-        self.animation_delay = 50  # Set the delay between animation frames in milliseconds
+        self.state = IdleState(self)
         super().__init__(*groups)
+
+    def __set_spaceship_state(self, state):
+        self.state = state
 
     def update(self, *args, **kwargs):
         self.__handle_movements()
@@ -34,13 +34,9 @@ class Spaceship(pygame.sprite.Sprite):
 
         # Check for left and right movement
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.tilting_left = True
-            self.rect.x -= self.move
-            self.__tilt_left()
+            self.__set_spaceship_state(TiltLeftState(self))
         elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.tilting_right = True
-            self.rect.x += self.move
-            self.__tilt_right()
+            self.__set_spaceship_state(TiltRightState(self))
 
         # Check for up and down movement
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
@@ -55,46 +51,7 @@ class Spaceship(pygame.sprite.Sprite):
 
         # If no keys pressed, return to idle state
         if not any(keys):
-            self.__return_to_idle()
-
-    def __return_to_idle(self):
-        current_time = pygame.time.get_ticks()  # Get the current time
-        if current_time - self.last_frame_update >= self.animation_delay:
-            self.index = 0
-            self.image = self.images[self.index]
-            if len(self.images) > 1:
-                self.index = 0
-                self.image = self.images[self.index]
-                self.image = pygame.transform.scale(assets.get_sprite('tile000'), (100, 100))
-            self.last_frame_update = current_time  # Update the last frame update time
-
-    def __tilt_left(self):
-        if not self.tilting_left:
-            return
-        current_time = pygame.time.get_ticks()  # Get the current time
-        if current_time - self.last_frame_update >= self.animation_delay:
-            self.images = [
-                pygame.transform.scale(assets.get_sprite('tile001l'), (100, 100)),
-                pygame.transform.scale(assets.get_sprite('tile002l'), (100, 100)),
-            ]
-            if self.index < len(self.images) - 1:
-                self.index += 1
-                self.image = self.images[self.index]
-            self.last_frame_update = current_time  # Update the last frame update time
-
-    def __tilt_right(self):
-        if not self.tilting_right:
-            return
-        current_time = pygame.time.get_ticks()  # Get the current time
-        if current_time - self.last_frame_update >= self.animation_delay:
-            self.images = [
-                pygame.transform.scale(assets.get_sprite('tile006r'), (100, 100)),
-                pygame.transform.scale(assets.get_sprite('tile007r'), (100, 100)),
-            ]
-            if self.index < len(self.images) - 1:
-                self.index += 1
-                self.image = self.images[self.index]
-            self.last_frame_update = current_time  # Update the last frame update time
+            self.__set_spaceship_state(IdleState(self))
 
     def __prevent_deserting(self):
         if self.rect.left <= 0:
@@ -113,3 +70,85 @@ class Spaceship(pygame.sprite.Sprite):
 
     def get_health(self):
         return self.health
+
+
+class SpaceshipState(ABC):
+    @abstractmethod
+    def fire(self, event=None): ...
+
+    @abstractmethod
+    def tilt_wing_left(self, event=None): ...
+
+    @abstractmethod
+    def tilt_wing_right(self, event=None): ...
+
+    @abstractmethod
+    def idle_state(self, event=None): ...
+
+
+class FireState(SpaceshipState):
+
+    def __init__(self, spaceship: Spaceship):
+        self.spaceship = spaceship
+
+    def tilt_wing_left(self, event=None):
+        pass
+
+    def tilt_wing_right(self, event=None):
+        pass
+
+    def idle_state(self, event=None):
+        pass
+
+    def fire(self, event=None):
+        pass
+
+
+class TiltLeftState(SpaceshipState):
+
+    def __init__(self, spaceship: Spaceship):
+        self.spaceship = spaceship
+        self.tilt_wing_left()
+
+    def fire(self, event=None): ...
+
+    def tilt_wing_left(self, event=None):
+        self.spaceship.image = self.spaceship.image = pygame.transform.scale(assets.get_sprite('tilt_left'), (100, 100))
+        self.spaceship.rect.x -= self.spaceship.move
+
+    def tilt_wing_right(self, event=None): ...
+
+    def idle_state(self, event=None): ...
+
+
+class TiltRightState(SpaceshipState):
+
+    def __init__(self, spaceship: Spaceship):
+        self.spaceship = spaceship
+        self.tilt_wing_right()
+
+    def fire(self, event=None): ...
+
+    def tilt_wing_left(self, event=None): ...
+
+    def tilt_wing_right(self, event=None):
+        self.spaceship.image = pygame.transform.scale(assets.get_sprite('tilt_right'), (100, 100))
+        self.spaceship.rect.x += self.spaceship.move
+
+    def idle_state(self, event=None): ...
+
+
+class IdleState(SpaceshipState):
+    def __init__(self, spaceship: Spaceship):
+        self.spaceship = spaceship
+        self.idle_state()
+
+    def fire(self, event=None): ...
+
+    def tilt_wing_left(self, event=None): ...
+
+    def tilt_wing_right(self, event=None): ...
+
+    def idle_state(self, event=None):
+        self.spaceship.image = pygame.transform.scale(assets.get_sprite('idle'), (100, 100))
+        self.spaceship.index = 0
