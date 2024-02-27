8f4e3b2a32576c5fa12f2dc7426867291c1f257a
Younes Halim <younes.halim@gmail.com>
2024-02-27 09:41:37 -0500

    added type for better readability

--- assets.py
+++ assets.py
@@ -1,11 +1,13 @@
 import os
 import pygame
+from pygame.mixer import Sound
+from pygame.sprite import Sprite
 
 sprites = {}
 audios = {}
 
 
-def load_sprites():
+def load_sprites() -> None:
     path = os.path.join("assets", "sprites")
     for file in os.listdir(path):
         try:
@@ -14,15 +16,15 @@ def load_sprites():
             continue
 
 
-def get_sprite(name):
+def get_sprite(name: str) -> Sprite:
     return sprites[name]
 
 
-def load_audios():
+def load_audios() -> None:
     path = os.path.join("assets", "audios")
     for file in os.listdir(path):
         audios[file.split('.')[0]] = pygame.mixer.Sound(os.path.join(path, file))
 
 
-def play_audio(name):
-    audios[name].play()
+def get_audio(name: str) -> Sound:
+    return audios[name]
