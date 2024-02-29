eea65bad786a29453f75c08dd312fa3138e3f63b
Younes Halim <younes.halim@gmail.com>
2024-02-28 21:30:42 -0500

    improved collision and added sound fx

--- game_sys/explosion_fx_sys.py
+++ game_sys/explosion_fx_sys.py
@@ -2,11 +2,16 @@ import pygame
 from pygame.sprite import AbstractGroup
 
 import assets
+from game_objects.asteroid import Asteroid
+from game_objects.sound import SoundFX
 from layer import Layer
 
 
-class ExplosionEffect(pygame.sprite.Sprite):
-    def __init__(self, *groups: AbstractGroup, x: int, y: int):
+class ExplosionEffect(pygame.sprite.Sprite, SoundFX):
+    __EXPLOSION__SOUND_FX = 'explosion'
+    __SPACESHIP_EXPLOSION_SOUND_FX = 'spaceship_explosion'
+
+    def __init__(self, *groups: AbstractGroup, collided):
         self._layer = Layer.EXPLOSION
         self.explosion_speed = 4
         terminate = False
@@ -21,13 +26,16 @@ class ExplosionEffect(pygame.sprite.Sprite):
                 counter += 1
             except KeyError:
                 terminate = True
-
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
+        x, y = collided.rect.centerx, collided.rect.centery
         self.rect.center = [x, y]
         self.mask = pygame.mask.from_surface(self.image)
         self.animation_speed = 0
+        self.sound_fx = assets.get_audio(self.__EXPLOSION__SOUND_FX) if type(collided) is Asteroid else assets.get_audio(self.__SPACESHIP_EXPLOSION_SOUND_FX)
+        pygame.mixer.music.set_volume(.7)
+        self.play()
         super().__init__(*groups)
 
     def update(self, *args, **kwargs):
@@ -48,3 +56,15 @@ class ExplosionEffect(pygame.sprite.Sprite):
         # if the animation is complete, reset animation index
         if self.index >= len(self.images) - 1 and self.animation_speed >= self.explosion_speed:
             self.kill()
+
+    def play(self):
+        self.sound_fx.play()
+
+    def pause(self):
+        pass
+
+    def fade_in(self):
+        pass
+
+    def fade_out(self):
+        pass
