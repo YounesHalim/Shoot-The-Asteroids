a013dbc71b85e134aa48c7a084cbb113e9a564f7
Younes Halim <younes.halim@gmail.com>
2024-03-01 11:38:37 -0500

    added sound fx for beams

--- game_objects/weapons.py
+++ game_objects/weapons.py
@@ -1,17 +1,25 @@
+import os
+
 import pygame.sprite
 from pygame.sprite import AbstractGroup
 
 import assets
+import configs
+from game_objects.sound import SoundFX
 from layer import Layer
 
 
-class LaserBeam(pygame.sprite.Sprite):
+class LaserBeam(pygame.sprite.Sprite, SoundFX):
+
+    __SOUND_FX = 'beam'
 
     def __init__(self, *groups: AbstractGroup, x=None, y=None):
         self._layer = Layer.WEAPON
-        self.image = pygame.transform.scale(assets.get_sprite('beam'), (64, 64))
+        self.image = pygame.transform.scale(assets.get_sprite('red-beam'), (64, 90))
         self.rect = self.image.get_rect(topleft=(x, y))
-        self.fps = 0
+        self.fps = configs.FPS
+        self.sound_fx = assets.get_audio(self.__SOUND_FX)
+        self.play()
         super().__init__(*groups)
 
     def update(self, *args, **kwargs):
@@ -35,3 +43,19 @@ class LaserBeam(pygame.sprite.Sprite):
         # remove the beam from memory if it's deserting. (going out of the y-axis)
         if self.rect.y <= -self.image.get_height():
             self.kill()
+
+    def play(self):
+        try:
+            self.sound_fx.play().set_volume(.5)
+        except AttributeError:
+            pass
+
+    def pause(self):
+        raise NotImplemented()
+
+    def fade_in(self):
+        raise NotImplemented()
+
+    def fade_out(self):
+        raise NotImplemented()
+
