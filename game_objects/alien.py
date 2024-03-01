251ebad37e97cf2ce123d74ba411424ed192a795
Younes Halim <younes.halim@gmail.com>
2024-03-01 11:41:59 -0500

    added velocity attribute to alien class

--- game_objects/alien.py
+++ game_objects/alien.py
@@ -6,7 +6,7 @@ from layer import Layer
 class Alien(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup):
         self._layer = Layer.ALIEN
-
+        self.vel = pygame.math.Vector2()
         super().__init__(*groups)
 
     def update(self, *args, **kwargs):
